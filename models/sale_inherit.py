639fd444f84f19d17938ea4acffd62fb3d1691a1
taufanhashmicro <Taufan.Abdillah.Harahap@hashmicro.com>
2022-04-26 09:21:30 +0700

    commit memperbaikkan function creater Auto MO

--- models/sale_inherit.py
+++ models/sale_inherit.py
@@ -6,25 +6,10 @@ class SaleInherit(models.Model):
 
     def create_mo(self):
         mo_auto = []
-        mo_auto_target = self.env['sale.order'].browse(self.id)
-        # print("a")
-        for m in mo_auto_target:
-            mo_auto.append([{
-                'product_id': m.order_line.product_id.id,
-                'product_uom_id': m.order_line.product_id.bom_ids.product_uom_id.id,
-                'bom_id': m.order_line.product_id.bom_ids.id,
-                'harga_bom': m.order_line.product_id.bom_ids.harga_bom,
-                'product_qty': m.order_line.product_uom_qty,
-            }])
+        mo_auto_target = self.env['mrp.bom'].search(self.product_id.id)
 
-        auto_product_id = mo_auto[0][0]["product_id"]
-        auto_product_uom_id = mo_auto[0][0]["product_uom_id"]
-        auto_bom_id = mo_auto[0][0]["bom_id"]
-        auto_harga_bom = mo_auto[0][0]["harga_bom"]
-        auto_product_qty = mo_auto[0][0]["product_qty"]
-
-        if auto_harga_bom:
+        if self.order_line.product_id.bom_ids.harga_bom > 0:
             self.env['mrp.production'].create(
-                {'product_id': auto_product_id, "product_uom_id": auto_product_uom_id, "bom_id": auto_bom_id, "product_qty": auto_product_qty})
+                {'product_id': self.order_line.product_id.id, "product_uom_id": self.order_line.product_uom_id.id, "bom_id": self.order_line.product_id.bom_ids.id, "product_qty": self.order_line.product_uom_qty})
         else:
             pass
