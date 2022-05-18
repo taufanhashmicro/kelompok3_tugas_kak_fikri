ae7a65931b0414cbb504ec79b36289ff5d382d5b
rafsanjaniHashmicro <rafsanjani.rahadi@hashmicro.com>
2022-05-18 21:53:52 +0700

    Membuat view wizard

--- wizard/bom_wizard.py
+++ wizard/bom_wizard.py
@@ -5,4 +5,19 @@ class BomWizard(models.TransientModel):
     _name = 'bom.wizard'
     _description = 'Wizard Information BoM'
 
-    bom_ids = fields.Char(string='MRP BoM')
+    # sale_order_compute = fields.Many2one("sale.order", string="Sale Order",
+    #     compute='_compute_field' )
+
+    # @api.depends('depends')
+    # def _compute_field(self):
+    #     return self.env['sale.order'].search([],limit=1, order='id desc')
+
+
+    # # bom_ids = fields.Many2one(comodel_name="mrp.bom",string='MRP BoM')
+    # produk_so = sale_order_compute.order_line
+
+    # for record in produk_so:
+    #         if record.product_id.bom_ids:
+    #             continue
+    #         else :
+    #             print('Mohon Maaf Produk Ini Tidak Mempunyai BoM')
