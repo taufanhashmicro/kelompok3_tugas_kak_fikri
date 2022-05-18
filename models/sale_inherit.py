0699b520f0a2235f6ea8da2224b6b1b8938a0a4b
SugengHashmicro <sugeng.iman.santosa@hashmicro.com>
2022-05-18 19:23:04 +0700

    setting wizard on button MO

--- models/sale_inherit.py
+++ models/sale_inherit.py
@@ -6,23 +6,23 @@ class SaleInherit(models.Model):
 
     def create_mo(self):
         for record in self.order_line:
-            for k in record.product_id.bom_ids:
-                auto_bom = record.env['mrp.bom'].search(
-                    [('id', '=', k.id)]).read()
-                if auto_bom == True:
-                    record.env['mrp.production'].create(
-                        {'product_id': record.product_id.id,
-                         "product_uom_id": record.product_uom.id,
-                         "bom_id": k.id,
-                         "product_qty": record.product_uom_qty})   
-                
-            return {
-                    'type': 'ir.actions.act_window',
-                    'name': 'Wizard BoM',
-                    'res_model': 'bom.wizard',
-                    'view_mode': 'form',
-                    'view_type': 'form',
-                    'target': 'new',
-                   }
+            if record.product_id.bom_ids:
+                for bom_id in record.product_id.bom_ids:
+                    bom_data = record.env['mrp.bom'].search([('id', '=', bom_id.id)]).read()
+                    if bom_data:
+                        record.env['mrp.production'].create({
+                            'product_id': record.product_id.id, 
+                            'product_uom_id': record.product_id.bom_ids.product_uom_id.id, 
+                            'bom_id': bom_id.id, 
+                            'product_qty': record.product_uom_qty})
+            else:
+                return {
+                        'type': 'ir.actions.act_window',
+                        'name': 'Wizard BoM',
+                        'res_model': 'bom.wizard',
+                        'view_mode': 'form',
+                        'view_type': 'form',
+                        'target': 'new',
+                        }
                
 
